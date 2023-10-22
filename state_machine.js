c3139f34b7a2462e422557dffbcaf23c50b28e5e
Ben Zhou <zhougy0717@gmail.com>
2023-10-22 19:32:29 +0800

    Refactor: Add old state and new state as state machine action args

--- state_machine.js
+++ state_machine.js
@@ -14,8 +14,9 @@ const stateTable = {
 let g_mode = "command"
 async function updateState (trigger, action) {
     if (trigger in stateTable[g_mode]) {
+        const oldState = g_mode
         g_mode = stateTable[g_mode][trigger]
-        await action()
+        await action(oldState, g_mode)
     }
 }
 
